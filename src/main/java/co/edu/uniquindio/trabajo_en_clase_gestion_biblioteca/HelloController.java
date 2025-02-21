a8f1db152a8db29f240d4b014af188bd61cc5f62
Andres David Parra <andresdavidparra@Andress-Laptop-2.local>
2025-02-21 08:31:43 -0500

    Primer commit del proyecto

---
+++ src/main/java/co/edu/uniquindio/trabajo_en_clase_gestion_biblioteca/HelloController.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.trabajo_en_clase_gestion_biblioteca;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module co.edu.uniquindio.trabajo_en_clase_gestion_biblioteca {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquindio.trabajo_en_clase_gestion_biblioteca to javafx.fxml;
+    exports co.edu.uniquindio.trabajo_en_clase_gestion_biblioteca;
+}
